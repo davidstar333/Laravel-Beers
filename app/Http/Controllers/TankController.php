11652c58f6c6d5cd0c617b04af9264c7ee98e3a0
phyzerbert <xian1017@outlook.com>
2019-05-02 03:26:31 +0800

    first commit

---
+++ app/Http/Controllers/TankController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Tank;
+
+class TankController extends Controller
+{
+    public function index(Request $request){
+        $tanks = Tank::all();
+        return response()->json($tanks);
+    }
+    
+    public function brew(Request $request){
+        $tank_id = $request->get('tank_id');
+        $amount = $request->get('amount');
+        $tank = Tank::find($tank_id);
+        $tank->increment('amount', $amount);
+        $tank->save();
+        return response()->json($tank);
+    }
+
+    public function move(Request $request){
+        $source_tank_id = $request->get('source_tank_id');
+        $target_tank_id = $request->get('target_tank_id');
+        $amount = $request->get('amount');
+        $source_tank = Tank::find($source_tank_id);
+        $source_amount = $source_tank->amount;
+        if(floatval($amount) > floatval($source_amount)){
+            echo "error";
+            exit;
+        }
+        $target_tank = Tank::find($target_tank_id);
+        $source_tank->decrement('amount', $amount);
+        $target_tank->increment('amount', $amount);
+        $data = array(
+            'source_tank' => $source_tank,
+            'target_tank' => $target_tank
+        );
+        $tank->save();
+        return response()->json($data);
+    }
+
+    public function dump(Request $request){
+        $tank_id = $request->get('tank_id');
+        $amount = $request->get('amount');
+        $tank = Tank::find($tank_id);
+        $source_amount = $tank->amount;
+        if(floatval($amount) > floatval($source_amount)){
+            echo "error";
+            exit;
+        }
+        $tank->decrement('amount', $amount);
+        $tank->save();
+        return response()->json($tank);
+    }
+
+    public function keg(Request $request){
+        $tank_id = $request->get('tank_id');
+        $amount = $request->get('amount');
+        $tank = Tank::find($tank_id);
+        $source_amount = $tank->amount;
+        if(floatval($amount) > floatval($source_amount)){
+            echo "error";
+            exit;
+        }
+        $tank->decrement('amount', $amount);
+        $tank->save();
+        return response()->json($tank);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/all', 'TankController@index')->name('index');
+Route::post('/brew', 'TankController@brew')->name('brew');
+Route::post('/move', 'TankController@move')->name('move');
+Route::post('/dump', 'TankController@dump')->name('dump');
+Route::post('/keg', 'TankController@keg')->name('keg');
